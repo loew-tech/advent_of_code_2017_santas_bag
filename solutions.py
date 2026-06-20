bde514e598b0d367dbcb447404ac00711bbf204d
loew-tech <loew.technology@gmail.com>
2026-06-20 11:00:11 -0600

    day 2 parts 1 & 2

--- solutions.py
+++ solutions.py
@@ -1,6 +1,7 @@
 import inspect
 import sys
-from typing import List
+from operator import floordiv, sub
+from typing import List, Callable, Any, Tuple
 
 from santas_bag.utils import read_input
 from santas_bag.parse import ints
@@ -9,13 +10,37 @@ from santas_bag.parse import ints
 with open('.env') as f:
     session_id = f.readlines()[0]
 
+def _read_input(day_: int,
+                delim: str | None = '\n',
+                parse: Callable[[List[str] | str], Any | List[Any] | str] | None = None) -> List | str | Any:
+    return read_input(2017, day_, session_id, delim=delim, parse=parse)
+
 
 def day_1(part_1=True) -> int:
-    data: List[int] = read_input(
-        2017, 1, session_id, delim=None, parse=lambda s: [int(i) for i in s.strip()]
-    )
+    data: List[int] = _read_input(1, delim=None, parse=lambda s: [int(i) for i in s.strip()])
     offset = 1 if part_1 else len(data) // 2
     return sum(v for i, v in enumerate(data) if v == data[i-offset])
+
+
+def day_2(part_1=True) -> int:
+    def parse_1(line: str) -> Tuple[int, int]:
+        ints_ = ints(line)
+        return max(ints_), min(ints_)
+
+    def parse_2(line: str) -> Tuple[int, int]:
+        ints_ = ints(line)
+        for i, v in enumerate(ints_):
+            for j in range(i+1, len(ints_)):
+                if not v % ints_[j]:
+                    return v,ints_[j]
+                elif not ints_[j] % v:
+                    return ints_[j],v
+        return 0, 1
+
+    data: List[Tuple[int, int]] = _read_input(2, parse=parse_1 if part_1 else parse_2)
+    op_ = sub if part_1 else floordiv
+    return sum(op_(mx, mn) for mx, mn in data)
+
 
 if __name__ == '__main__':
     args = (f'day_{i}' for i in (sys.argv[1:] if
